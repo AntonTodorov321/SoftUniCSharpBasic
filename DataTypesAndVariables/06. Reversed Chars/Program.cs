eda074eceb1339cec20e1534507ae49d4d51b8fe
Anton Todorov <antontodorov321@gmail.com>
2023-07-09 13:48:01 +0300

    Add DataTypesAndVariables

---
+++ DataTypesAndVariables/06. Reversed Chars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _06._Reversed_Chars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char someChar1 = char.Parse(Console.ReadLine());
+			char someChar2 = char.Parse(Console.ReadLine());
+			char someChar3 = char.Parse(Console.ReadLine());
+			Console.Write($"{someChar3} {someChar2} {someChar1}");
+		}
+	}
+}
---
+++ DataTypesAndVariables/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int endTarget = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= endTarget; num++)
+            {
+                int currentNum = num;
+                int totalSum = 0;
+                while (currentNum > 0)
+                {
+                    totalSum += currentNum % 10;
+                    currentNum /= 10;
+                }
+                bool isUpper = false;
+                isUpper = (totalSum == 5) || (totalSum == 7) || (totalSum == 11);
+                Console.WriteLine("{0} -> {1}", num, isUpper);
+            }
+
+        }
+    }
+}
